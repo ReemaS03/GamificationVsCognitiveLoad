561df54fbb05a4bac0442176253a1d70cb269185
ReemaS03 <ralsaeed@ucsd.edu>
2025-03-03 12:58:23 -0800

    created writeup.html and added a nav bar

---
+++ navbar.js
@@ -0,0 +1,25 @@
+let pages = [
+    { url: 'index.html', title: 'Home' },
+    { url: 'writeup.html', title: 'Writeup' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+}
+
